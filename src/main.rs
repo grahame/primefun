6c0b27e2c07c8abeae27d871dcccb4437381c9a4
Grahame Bowland <grahame@bowland.au>
2025-10-07 09:26:24 +0800

    speed logging, trim output to > 6

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::thread;
 
 use num_prime::nt_funcs::{factorize, is_prime};
 use num_prime::{PrimalityTestConfig, RandPrime};
+use std::time::Instant;
 
 fn cunningham_kind1(start: u128) -> u8 {
     fn seek_back(mut k: u128) -> u128 {
@@ -18,7 +19,7 @@ fn cunningham_kind1(start: u128) -> u8 {
             k = k2;
         }
         return k;
-    };
+    }
 
     let mut n: u8 = 1;
     let mut x: u128 = seek_back(start);
@@ -42,21 +43,30 @@ fn cunningham_kind1(start: u128) -> u8 {
 
 fn main() {
     let best = 2759832934171386593519u128;
+    const BLOCK_SIZE: u32 = 1_000_000;
 
     let num_threads = 8;
     let mut handles = Vec::new();
-    for _i in 0..num_threads {
+    for task_id in 0..num_threads {
         let handle = thread::spawn(move || {
             let mut rng = rand::thread_rng();
 
             loop {
-                let p1: u128 = rng.gen_prime(75, None);
-                if p1 > best {
-                    let c = cunningham_kind1(p1);
-                    if c > 4 {
-                        println!("{} {}", c, p1);
+                let now = Instant::now();
+                for _i in 0..BLOCK_SIZE {
+                    let p1: u128 = rng.gen_prime(75, None);
+                    if p1 > best {
+                        let c = cunningham_kind1(p1);
+                        if c > 6 {
+                            println!("{} {}", c, p1);
+                        }
                     }
                 }
+                eprintln!(
+                    "rate[{}]: {:.2?}/prime",
+                    task_id,
+                    now.elapsed() / BLOCK_SIZE
+                );
             }
         });
         handles.push(handle);
